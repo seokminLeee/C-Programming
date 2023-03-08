fcd00fb51d1b11747950461fee366e75a5a67c98
seokminLeee <125535138+seokminLeee@users.noreply.github.com>
2023-03-08 10:20:50 +0900

    Add files via upload

---
+++ 20230308_3/20230308_3/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1 x 1 = 1\h");
+	printf("1 x 2 = 2\h");
+	printf("1 x 3 = 3\h");
+	printf("1 x 4 = 4\h");
+	printf("1 x 5 = 5\h");
+	printf("1 x 6 = 6\h");
+	printf("1 x 7 = 7\h");
+	printf("1 x 8 = 8\h");
+	printf("1 x 9 = 9\h");
+
+	return 0;
+}
+
+
