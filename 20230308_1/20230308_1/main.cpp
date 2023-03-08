8bfcd14fddc931a578de3ac1ca01983de2760cbb
seokminLeee <125535138+seokminLeee@users.noreply.github.com>
2023-03-08 10:13:54 +0900

    Add files via upload

---
+++ 20230308_1/20230308_1/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>  // header 파일추가 standrd ID
+
+
+int main(void)
+{
+
+    printf("test  \n");  // \n line feed
+    printf("202315014\n");  // \n line feed
+    printf("SeokMinLEE\n");  // \n line feed
+    return 0;
+}
+
+  //ctrl+F5 는 실행
+
+
+
+
+
+
+
+
+
---
+++ 20230308_3/20230308_3/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+	printf("1 x 1 = 1\h");
+
+	return 0;
+}
+
+
